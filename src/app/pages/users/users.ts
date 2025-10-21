e68e2525f0b038285e962442ec070f2bfb5d3d24
Aleksandre-Mataradze <mataradze.alex@gmail.com>
2025-10-21 14:48:16 +0400

    complete users page with API integration and card layout

--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
-export class Users {
+export class Users implements OnInit {
 
+  users = signal<any[]>([]);
+
+  private usersService = inject(UsersService);
+
+  ngOnInit(): void {
+    this.usersService.getUsers()
+    .subscribe({
+      next: (data) => {
+        console.log(data);
+        this.users.set(data);
+      },
+      error: (error) => console.error(error)
+    });
+  }
+
+  trackByID(index: number, user: any): number {
+    return user.id
+  }
 }
