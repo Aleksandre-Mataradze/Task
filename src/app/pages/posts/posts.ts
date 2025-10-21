b67a21a8e239cbf73a4cb3fbfe69018779ccd47d
Aleksandre-Mataradze <mataradze.alex@gmail.com>
2025-10-21 16:32:44 +0400

    complete posts page with API integration and card layout

--- src/app/pages/posts/posts.ts
+++ src/app/pages/posts/posts.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts.html',
   styleUrl: './posts.scss'
 })
 export class Posts {
+  
+  posts = signal<any[]>([]);
 
+  private postService = inject(PostsService);
+
+  ngOnInit(): void {
+    this.postService.getUsers()
+    .subscribe({
+      next: (data) => {
+        console.log(data);
+        this.posts.set(data);
+      },
+      error: (error) => console.error(error)
+    });
+  }
+
+  trackByID(index: number, post: any): number {
+    return post.id
+  }
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PostsService {
+
+    private http = inject(HttpClient);
+
+    getUsers(): Observable<any> {
+        return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    }
+}
